c195104bee26ef683241e32ad223cc45fee11541
xiangjie256329@163.com <xiangjie256329@163.com>
2023-03-21 11:22:35 +0800

    line data

--- test/core/Vault/helpers.js
+++ test/core/Vault/helpers.js
@@ -129,8 +129,8 @@ function getBtcConfig(btc, btcPriceFeed) {
     btc.address, // _token
     8, // _tokenDecimals
     10000, // _tokenWeight
-    75, // _minProfitBps
-    0, // _maxUsdgAmount
+    0, // _minProfitBps
+    95000000000000000000000000, // _maxUsdgAmount
     false, // _isStable
     true // _isShortable
   ]
@@ -141,8 +141,8 @@ function getDaiConfig(dai, daiPriceFeed) {
     dai.address, // _token
     18, // _tokenDecimals
     10000, // _tokenWeight
-    75, // _minProfitBps
-    0, // _maxUsdgAmount
+    0, // _minProfitBps
+    35000000000000000000000000, // _maxUsdgAmount
     true, // _isStable
     false // _isShortable
   ]
